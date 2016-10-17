7c654e6719005225d20d2a423032fc99cbaa1840
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-10-16 17:46:24 -0700

    Added English versions of card name and rank

--- cards.cpp
+++ cards.cpp
@@ -110,13 +110,63 @@ string Card::get_spanish_rank() const {
 // Accessor: returns a string with the suit of the card in English 
 // This is just a stub! Modify it to your liking.
 string Card::get_english_suit() const { 
-   return "";
+	string suitName;
+   switch (suit) {
+      case OROS: 
+         suitName = "golds"; 
+	 break;
+      case COPAS: 
+         suitName = "cups"; 
+	 break; 
+      case ESPADAS: 
+         suitName = "swords"; 
+	 break;
+      case BASTOS: 
+         suitName = "clubs"; 
+	 break; 
+      default: break;
+   }
+   return suitName;
 }
 
 // Accessor: returns a string with the rank of the card in English 
 // This is just a stub! Modify it to your liking.
 string Card::get_english_rank() const { 
-   return "";
+   string rankName;
+   switch (rank) {
+      case AS:
+         rankName = "Ace"; 
+	 break; 
+      case DOS: 
+         rankName = "Two"; 
+	 break; 
+      case TRES: 
+         rankName = "Three"; 
+	 break; 
+      case CUATRO: 
+         rankName = "Four"; 
+	 break; 
+      case CINCO: 
+         rankName = "Five"; 
+	 break; 
+      case SEIS: 
+         rankName = "Six"; 
+	 break; 
+      case SIETE: 
+         rankName = "Seven"; 
+	 break; 
+      case SOTA: 
+         rankName = "Jack"; 
+	 break; 
+      case CABALLO: 
+         rankName = "Knight"; 
+	 break; 
+      case REY: 
+         rankName = "King"; 
+	 break; 
+      default: break;
+   }
+   return rankName;
 }
 
 

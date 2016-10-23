5df4ffff34395a54ac194bbd990368e7c1a98ac9
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-10-23 15:23:15 -0700

    Defined extra useful functions

--- cards.h
+++ cards.h
@@ -1,8 +1,7 @@
 /* *************************************
-   Ricardo Salazar, February 26, 2015
-
-   Interface of a simple Card class
-   ************************************* */
+Ricardo Salazar, February 26, 2015
+Interface of a simple Card class
+************************************* */
 
 #include <string>
 #include <vector>
@@ -13,81 +12,88 @@
 
 using namespace std;
 
-enum suit_t {OROS, COPAS, ESPADAS, BASTOS};
+enum suit_t { OROS, COPAS, ESPADAS, BASTOS };
 
-   /*
-   The values for this type start at 0 and increase by one 
-   afterwards until they get to SIETE. 
-   The rank reported by the function Card::get_rank() below is 
-   the value listed here plus one.
-   E.g: 
-   The rank of AS is reported as    static_cast<int>(AS) + 1   = 0 + 1 =  1
-   The rank of SOTA is reported as  static_cast<int>(SOTA) + 1 = 9 + 1 = 10
-   */
-enum rank_t {AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA=9, CABALLO=10, REY=11};
+/*
+The values for this type start at 0 and increase by one
+afterwards until they get to SIETE.
+The rank reported by the function Card::get_rank() below is
+the value listed here plus one.
+E.g:
+The rank of AS is reported as    static_cast<int>(AS) + 1   = 0 + 1 =  1
+The rank of SOTA is reported as  static_cast<int>(SOTA) + 1 = 9 + 1 = 10
+*/
+enum rank_t { AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA = 9, CABALLO = 10, REY = 11 };
 
 class Card {
-   public: 
-      // Constructor assigns random rank & suit to card.
-      Card();
-      
-      // Accessors 
-      string get_spanish_suit() const;
-      string get_spanish_rank() const; 
+public:
+	// Constructor assigns random rank & suit to card.
+	Card();
 
-      /* 
-         These are the only functions you'll need to code 
-         for this class. See the implementations of the two 
-	 functions above to get an idea of how to proceed.
-      */
-      string get_english_suit() const;
-      string get_english_rank() const; 
+	// Accessors 
+	string get_spanish_suit() const;
+	string get_spanish_rank() const;
 
-      // Converts card rank to number.
-      // The possible returns are: 1, 2, 3, 4, 5, 6, 7, 10, 11 and 12
-      int get_rank() const;
+	/*
+	These are the only functions you'll need to code
+	for this class. See the implementations of the two
+	functions above to get an idea of how to proceed.
+	*/
+	string get_english_suit() const;
+	string get_english_rank() const;
 
-      // Compare rank of two cards. E.g: Eight<Jack is true.
-      // Assume Ace is always 1. 
-      // Useful if you want to sort the cards.
-      bool operator < (Card card2) const;
+	// Converts card rank to number.
+	// The possible returns are: 1, 2, 3, 4, 5, 6, 7, 10, 11 and 12
+	double get_rank() const;
+
+	// Compare rank of two cards. E.g: Eight<Jack is true.
+	// Assume Ace is always 1. 
+	// Useful if you want to sort the cards.
+	bool operator < (Card card2) const;
 
 private:
-      suit_t suit;
-      rank_t rank;
+	suit_t suit;
+	rank_t rank;
 };
 
 
 class Hand {
-   public:
-      // A vector of Cards
-      Hand();
+public:
+	// A vector of Cards
+	Hand();
 
-      void push_back(Card c);
-      void reshuffle();
-      // You decide what functions you'll need...
+	void push_back(Card c);
+	void reshuffle();
+	vector<Card> get_cur_hand();
+	double get_total();
+	void add_to_total(double add);
+	void reset_total();
+	// You decide what functions you'll need...
 
-   private:
+private:
 	vector<Card> cur_hand;
-      // You decide what fields you'll need...
+	double total;
+	// You decide what fields you'll need...
 };
 
 
 class Player {
-   public:
-      // Constructor. 
-      //    Assigns initial amount of money
-      Player(int m);
-	
-      int get_money();
-      void subtract_bet(int bet);
-      void collect_prize(int prize);
-      // You decide what functions you'll need...
+public:
+	// Constructor. 
+	//    Assigns initial amount of money
+	Player(int m);
 
-   private:
-      int money;
-      int rounds = 0;
-      // You decide what extra fields (if any) you'll need...
+	int get_money();
+	void subtract_bet(int bet);
+	void collect_prize(int prize);
+	int get_rounds();
+	void increment_rounds();
+	// You decide what functions you'll need...
+
+private:
+	int money;
+	int rounds = 0;
+	// You decide what extra fields (if any) you'll need...
 };
 
 #endif

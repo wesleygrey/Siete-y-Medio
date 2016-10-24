9d3ec18a2a91b53e2bc0e272dcfe1870ecf74c8d
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-10-23 17:55:58 -0700

    Commented code

--- hw1.cpp
+++ hw1.cpp
@@ -1,4 +1,18 @@
-/*/** insert description later*/
+/**
+Wes Hartmann - PIC 10C
+Professor R. Salazar
+24 October 2016
+
+This is the main file of the implementation of the game 'Sieto y Medio'
+It allows the user to place bets and request or deny new cards. The player
+will place a bet, evaluate his or her hand and decide if another card is
+neccessary. After the player denies a card or busts, the dealer will begin
+collecting cards. The player wins by not busting and having a higher point
+total than the dealer.
+
+The player begins with $100
+Gameplay continues until the player has $0, or the dealer loses $900.
+*/
 
 #include <iostream>
 #include <vector>
@@ -15,6 +29,7 @@ int main() {
 	Player player(100);
 	Hand my_hand;
 	Hand dealer_hand;
+	//tracks how much money the dealer loses
 	int dealer_losses = 0;
 	int bet = 0;
 	ofstream gamelog;
@@ -23,6 +38,8 @@ int main() {
 
 
 newRound:
+	//exacutes after the first round has finished
+	//stores information about the most recent round in the file gamelog.txt
 	if (player.get_rounds() != 0) {
 		gamelog << "-----------------------------------------------" << std::endl << std::endl;
 		gamelog << "Game number: " << player.get_rounds();
@@ -48,7 +65,8 @@ newRound:
 		}
 		gamelog << "Dealer's total: " << dealer_hand.get_total() << "." << std::endl << std::endl;
 	}
-
+	
+	//clears both hands and sets totals back to 0
 	my_hand.reshuffle(); dealer_hand.reshuffle();
 
 	while ((player.get_money() > 0 && dealer_losses < MAX_DEALER_LOSSES) || player.get_rounds() == 0) {
@@ -58,6 +76,7 @@ newRound:
 		do {
 			std::cout << "You have $" << player.get_money() << ". Enter bet: ";
 			std::cin >> bet;
+			//insures player does not bet more money than they have
 			if (bet > player.get_money()) {
 				std::cout << "You don't have $" << bet << " to bet. Try again." << std::endl;
 				bet = 0;
@@ -67,7 +86,8 @@ newRound:
 		Card first_card;
 		my_hand.push_back(first_card);
 		my_hand.add_to_total(first_card.get_rank());
-
+		
+		//executes while player wants another card and has not breached 7.5 points
 		while (another_card == 'y' && my_hand.get_total() <= MAX_HAND_TOTAL) {
 
 			if (my_hand.get_cur_hand().size() != 1) {
@@ -77,10 +97,12 @@ newRound:
 				std::cout << std::setw(20)
 					<< "(" << my_hand.get_cur_hand().back().get_english_rank() << " of "
 					<< my_hand.get_cur_hand().back().get_english_suit() << ")" << std::endl << std::endl;
+				//left out the sort function because it caused program to fail
 				//std::sort(my_hand.get_cur_hand().begin(), my_hand.get_cur_hand().end());
 			}
 
 			std::cout << "Your cards:" << std::endl;
+			//prints to the screen the cards in the player's hand. Formatting errors
 			for (size_t pos = 0; pos != my_hand.get_cur_hand().size(); ++pos) {
 				std::cout << std::setfill(' ') << std::setw(20);
 				std::cout << my_hand.get_cur_hand()[pos].get_spanish_rank() << " de " << my_hand.get_cur_hand()[pos].get_spanish_suit();
@@ -91,7 +113,8 @@ newRound:
 
 			std::cout << "Your total is " << my_hand.get_total() << ". Do you want another card? (y/n) ";
 			std::cin >> another_card;
-
+			
+			//adds new card to the back of the hand
 			if (another_card == 'y') {
 				Card* next_card = new Card;
 				my_hand.push_back(*next_card);
@@ -99,15 +122,19 @@ newRound:
 				delete next_card;
 			}
 		}
-
+		
+		//executes after player has full hand. Populates dealer's hand
 		while (dealer_hand.get_total() <= MAX_HAND_TOTAL) {
-
+			
+			//executes if dealer's total surpasses player's total
 			if (dealer_hand.get_total() > my_hand.get_total()) {
 				std::cout << "Too bad. You lose $" << bet << std::endl << std::endl;
 				player.subtract_bet(bet);
 				player.increment_rounds();
+				//begins a new round of play
 				goto newRound;
 			}
+			//executes if the round is a draw
 			else if (dealer_hand.get_total() == my_hand.get_total()) {
 				std::cout << "Nobody wins!" << std::endl << std::endl;
 				player.increment_rounds();
@@ -141,6 +168,7 @@ newRound:
 			std::cout << "The dealer's total is " << dealer_hand.get_total() << std::endl << std::endl;
 
 			if (dealer_hand.get_total() > MAX_HAND_TOTAL) {
+				//executes if dealer busts but player total remians under 7.5
 				if (my_hand.get_total() <= MAX_HAND_TOTAL) {
 					std::cout << "You win $" << bet << std::endl << std::endl;
 					player.collect_prize(bet);
@@ -148,6 +176,7 @@ newRound:
 					player.increment_rounds();
 					goto newRound;
 				}
+				//executes if both dealer and player bust
 				else if (my_hand.get_total() > MAX_HAND_TOTAL) {
 					std::cout << "House advantage! Too bad. You lose $" << bet << std::endl << std::endl;
 					player.subtract_bet(bet);
@@ -157,7 +186,8 @@ newRound:
 			}
 		}
 	}
-
+	
+	//farewell messages will be printed to screen upon completion of a game of sieto y medio
 	if (player.get_money() >= 0) {
 		std::cout << "You have $0. GAME OVER!" << std::endl
 			<< "Come back when you have more money." << std::endl << std::endl
@@ -167,6 +197,7 @@ newRound:
 		std::cout << "Congratulations! You beat the casino!" << std::endl << std::endl
 			<< "Bye!" << std::endl;
 	}
+	//gamelog formatting
 	gamelog << "-----------------------------------------------";
 	gamelog.close();
 }
